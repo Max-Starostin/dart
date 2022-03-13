5879863656a2128b1beddb70c88cd494cdd81b4c
BlurryMaxStar <63944902+Max-Starostin@users.noreply.github.com>
2022-03-13 18:46:10 +0400

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -3,11 +3,14 @@
 const openButtonBurger = document.querySelector('.burger');
 const menuBurger = document.querySelector('.menu');
 const closeButtonBurger = document.querySelector('.menu__close')
+const siteBody = document.querySelector('body');
 
 openButtonBurger.addEventListener('click', () => {
     menuBurger.classList.add('active');
+    siteBody.classList.add('hidden');
 })
 
 closeButtonBurger.addEventListener('click', () => {
     menuBurger.classList.remove('active');
-})
+    siteBody.classList.remove('hidden');
+});
