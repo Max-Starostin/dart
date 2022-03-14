36f4924a0b9327f031d621533a4ecd183f07781c
BlurryMaxStar <63944902+Max-Starostin@users.noreply.github.com>
2022-03-14 21:44:51 +0400

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -14,3 +14,38 @@ closeButtonBurger.addEventListener('click', () => {
     menuBurger.classList.remove('active');
     siteBody.classList.remove('hidden');
 });
+
+const activeTabFirst = document.querySelector('#first-item');
+const activeTabDescriptionFirst = document.querySelector('#first__description');
+
+activeTabFirst.addEventListener('click', () => {
+    activeTabDescriptionFirst.classList.add('active');
+})
+
+const activeTabSecond = document.querySelector('#second-item');
+const activeTabDescriptionSecond = document.querySelector('#second__description');
+
+activeTabSecond.addEventListener('click', () => {
+    activeTabDescriptionSecond.classList.add('active');
+})
+
+const activeTabThird = document.querySelector('#third-item');
+const activeTabDescriptionThird = document.querySelector('#third__description');
+
+activeTabThird.addEventListener('click', () => {
+    activeTabDescriptionThird.classList.add('active');
+})
+
+const activeTabFourst = document.querySelector('#fourst-item');
+const activeTabDescriptionFourst = document.querySelector('#fourst__description');
+
+activeTabFourst.addEventListener('click', () => {
+    activeTabDescriptionFourst.classList.add('active');
+})
+
+const activeTabFivest = document.querySelector('#fivest-item');
+const activeTabDescriptionFivest = document.querySelector('#fivest__description');
+
+activeTabFivest.addEventListener('click', () => {
+    activeTabDescriptionFivest.classList.add('active');
+})
